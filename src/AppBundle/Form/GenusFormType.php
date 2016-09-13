d4411686932bc38809e084da760be92961e6967d
Katie Ude <kude@caxy.com>
2016-09-13 15:10:11 -0500

    new verison of my first Symfony3 project with forms tackled an completed

---
+++ src/AppBundle/Form/GenusFormType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\SubFamily;
+use AppBundle\Repository\SubFamilyRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GenusFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('subFamily', EntityType::class, array(
+                'class' => SubFamily::class,
+                'placeholder' => 'Choose a Sub Family',
+                'query_builder' => function(SubFamilyRepository $repo) {
+                    return $repo->createAlphabeticalQueryBuilder();
+                }
+            ))
+            ->add('speciesCount')
+            ->add('funFact')
+            ->add('isPublished', ChoiceType::class, array(
+                'choices' => array(
+                    'Yes' => true,
+                    'No' => false,
+                )
+            ))
+            ->add('firstDiscoveredAt', DateType::class, array(
+                'widget' => 'single_text',
+                'attr' => array(
+                    'class' => 'js-datepicker'
+                ),
+                'html5' => false
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Genus'
+        ]);
+    }
+
+    public function getName()
+    {
+        return 'app_bundle_genus_form_type';
+    }
+}
